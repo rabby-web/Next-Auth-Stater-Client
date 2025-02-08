f4581ba9c91ef0ce7fc30791d9d23c5553d8821f
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-08 08:54:12 +0600

    successfully google login successfully

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,14 +1,29 @@
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">
-        Welcome To {session?.user?.name}
-      </h1>
+      {session?.user && (
+        <>
+          <Image
+            src={session?.user?.image}
+            width={170}
+            height={170}
+            alt="user-img"
+            className="mx-auto border rounded-full my-5"
+          />
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            User Email: {session?.user?.email}
+          </h1>
+        </>
+      )}
     </div>
   );
 };
