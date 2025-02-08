81d2c5af4dea23473e61859ffbd200115939db5f
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-08 12:53:05 +0600

    eslint problem fixt

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { loginUser } from "@/utils/actions/loginUser";
 import { signIn } from "next-auth/react";
