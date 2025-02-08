f3ff89090f6ef6a5efe29a195538ee02cadb71e8
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-08 09:49:31 +0600

    success fully login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { loginUser } from "@/utils/actions/loginUser";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
-type FormValues = {
+export type FormValues = {
   email: string;
   password: string;
 };
@@ -16,8 +18,22 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
+  const router = useRouter();
+
   const onSubmit = async (data: FormValues) => {
-    console.log(data);
+    // console.log(data);
+    try {
+      const res = await loginUser(data);
+      console.log(res);
+      if (res.accessToken) {
+        alert(res.message);
+        localStorage.setItem("accessToken", res.accessToken);
+        router.push("/");
+      }
+    } catch (err: any) {
+      console.error(err.message);
+      throw new Error(err.message);
+    }
   };
 
   return (
